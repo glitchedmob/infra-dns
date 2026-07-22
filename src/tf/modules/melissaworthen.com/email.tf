4ece218d403a7d2807e7d84ea070a5a5944a1d86
Levi Zitting <me@levizitting.com>
2026-07-22 07:30:23 -0700

    fix: dkim strict for melissaworthen.com

--- src/tf/modules/melissaworthen.com/email.tf
+++ src/tf/modules/melissaworthen.com/email.tf
@@ -44,7 +44,7 @@ resource "cloudflare_dns_record" "melissaworthen_com_dmarc" {
   zone_id = var.zone_id
   name    = "_dmarc"
   type    = "TXT"
-  content = "v=DMARC1; p=none; pct=100; rua=mailto:re+vkpmbl0w3qm@dmarc.postmarkapp.com; sp=none; aspf=r;"
+  content = "v=DMARC1; p=none; pct=100; rua=mailto:re+vkpmbl0w3qm@dmarc.postmarkapp.com; sp=none; adkim=s; aspf=r;"
   comment = var.comment
   proxied = false
   ttl     = 1
